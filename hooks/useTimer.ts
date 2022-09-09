a987f6a818cdb7625de442563ad7f858a25d5721
Lucas Pinheiro Moraes <lucas3.14nheiro@gmail.com>
2022-09-08 23:16:54 -0300

    ✨ timer working

--- hooks/useTimer.ts
+++ hooks/useTimer.ts
@@ -1,17 +1,43 @@
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
+
+// interface Timer {
+//   run: boolean;
+//   counter: number;
+// }
 
 interface TimerOptions {
   start: Date;
   end: Date;
 }
 
-enum TimerType {
-  increment,
-  decrement,
-}
+type TimerType = "increment" | "decrement";
 
 export default function useTimer(type: TimerType, options: TimerOptions) {
-  const [counter, setCounter] = useState();
-  const [run, setRun] = useState();
-  const stopTimer = () => {};
+  const interval_time = 1000;
+  const [counter, setCounter]: any = useState(0);
+  const [running, setRunning] = useState(false);
+  useEffect(() => {
+    console.log(options);
+    const delta = options.end.valueOf() - options.start.valueOf();
+    setCounter(delta < 0 ? 0 : delta);
+  }, []);
+  useEffect(() => {
+    running && counter > 0 && setTimeout(() => updateCounter(), interval_time);
+  }, [counter]);
+  const reduceCounter = () => {
+    setCounter((prev) => prev - interval_time);
+  };
+  const increaseCounter = () => {
+    setCounter((prev) => prev + interval_time);
+  };
+  const updateCounter = () => {
+    type === "decrement" ? reduceCounter() : increaseCounter();
+  };
+  const startTimer = () => {
+    setRunning(true);
+  };
+  const stopTimer = () => {
+    setRunning(false);
+  };
+  return { counter, startTimer, stopTimer };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,30 @@
 import clsx from "clsx";
 import styles from "../styles/Home.module.css";
 
-// import { targetDate } from "../config";
-// import useTimer from "@hooks/useTimer";
+import { targetDate } from "../config/index";
+import useTimer from "../hooks/useTimer";
+import { useEffect } from "react";
+
+const addZero = (num) => num < 10 ? "0" + num : num;
 
 export default function Home() {
   const today = new Date(Date.now());
-  // const end = targetDate;  
+  const end = targetDate;
+  const { counter, startTimer } = useTimer("decrement", {
+    start: today,
+    end: end,
+  });
+  useEffect(() => {
+    startTimer();
+  }, []);
+  const seconds = Math.floor((counter / 1000) % 60);
+  const minutes = Math.floor((counter / (60 * 1000)) % 60);
+  const hours = Math.floor(counter / (3600 * 1000));
 
   return (
     <div className="flex w-screen h-screen items-center justify-center">
       <h1 className={clsx(styles["time-counter"], "text-9xl text-gray-300")}>
-        00:00:00
+        {addZero(hours)}:{addZero(minutes)}:{addZero(seconds)}
       </h1>
     </div>
   );
