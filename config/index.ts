470046e455abb52c46f1f3646a1489ffa3f5fd4f
Lucas Pinheiro Moraes <47045278+mecharazick@users.noreply.github.com>
2022-09-27 13:39:46 -0300

    :wrench: updated target date to October thirteenth

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,2 @@
 // 8/20/2022, 6:00:00 AM - TimeZone: America/Sao_Paulo
-export const targetDate = new Date("2022-10-20T06:00:00-03:00");
+export const targetDate = new Date("2022-10-13T06:00:00-03:00");
